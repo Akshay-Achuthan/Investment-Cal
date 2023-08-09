46507ef18671b8bfb581f9058910fd59505d5331
akshayachuthan <akshayacuthan@credenceanalytics.com>
2023-08-09 23:46:29 +0530

    removed comments and bug fix

--- src/App.js
+++ src/App.js
@@ -4,27 +4,23 @@ import InvestmentLogo from "./Components/Investment/InvestmentLogo"
 import InvestmentForm from "./Components/Investment/InvestmentForm";
 import InvestmentTable from "./Components/Table/InvestmentTable";
 
-const yearlyData = []; // per-year results
+const yearlyData = []; 
 
 function App() {
 
   const [yearlyRequiredData,setYearlyRequiredData] = useState(yearlyData);
   
   const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
 
-    let currentSavings = userInput.currentSaving; // feel free to change the shape of this input object!
-    const yearlyContribution = userInput.yearlyContribution; // as mentioned: feel free to change the shape...
+    let currentSavings = userInput.currentSaving; 
+    const yearlyContribution = userInput.yearlyContribution; 
     const expectedReturn = userInput.expectedReturn / 100;
     const duration = userInput.duration;
 
-    // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
@@ -33,20 +29,14 @@ function App() {
     }
 
     setYearlyRequiredData( (prevData) => {
-      return [...prevData,yearlyRequiredData]
+      return [...prevData, ...yearlyRequiredData]
     })
-    // console.log(yearlyData);
-
-    // do something with yearlyData ...
   };
 
   return (
     <div>
       <InvestmentLogo/>
       <InvestmentForm onFormSubmit={calculateHandler}/>
-
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
 
       {yearlyRequiredData.length > 0 ? <InvestmentTable tableData={yearlyRequiredData}/> : <h2 className="no-data">No Data Found</h2>}
 
